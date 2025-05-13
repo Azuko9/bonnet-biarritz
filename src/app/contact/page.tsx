f9b66463f693e578e92ee4909b429b38d5ed43f0
Jean Luc Cazalaa <jcazalaa@bonnet-biarritz.fr>
2025-05-13 17:30:42 +0200

    Add Contact page with styles and background image

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,6 +9,7 @@ const ContactPage: React.FC = () => {
       <main className={styles.main}>
         <h1>Contact</h1>
         <p>Bienvenue sur la page de contact !</p>
+
         <iframe
           className={styles.map}
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9739.438984954799!2d-1.5490222889991294!3d43.469938220032326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd511535d6b12d8d%3A0x531d06240053c307!2sBONNET%20Magasin%20TV%20-%20HIFI%20-%20VIDEO!5e0!3m2!1sfr!2sfr!4v1745420942696!5m2!1sfr!2sfr"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-
 export default function Home() {
-  return <div className={styles.main}>HELLO</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.marque}>sony</div>
+      <div className={styles.marque}>sony</div>
+      <div className={styles.marque}>sony</div>
+      <div className={styles.marque}>sony</div>
+      <div className={styles.marque}>sony</div>
+      <div className={styles.marque}>sony</div>
+    </div>
+  );
 }
